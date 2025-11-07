ceb7ae90f640cc67e35a4961c477c5b0b360210e
Ezedin Ahmed <ezedinadefa@gmail.com>
2025-11-07 16:31:00 +0300

    command not found

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
     System.out.print("$ ");
+    Scanner scanner = new Scanner(System.in);
+    String command = scanner.nextLine();
+    System.out.print(command + ": command not found");
     }
 }
