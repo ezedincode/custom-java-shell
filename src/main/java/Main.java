58cbd1e6a8c7f3db82ec78cda6f7585f76d99da1
Ezedin Ahmed <ezedinadefa@gmail.com>
2025-11-07 16:39:25 +0300

    implemnting RPEL

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ public class Main {
             System.out.print("$ ");
             Scanner scanner = new Scanner(System.in);
             String command = scanner.nextLine();
-            System.out.print(command + ": command not found");
+            System.out.println(command + ": command not found");
         }
 
     }
